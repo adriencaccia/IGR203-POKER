0daa6f50273834d3dd9460e240d2769a7f0019b3
Henri <henrilepage@hotmail.fr>
2018-04-05 21:21:11 +0200

    le bon commit.

--- poker/client_src/src/components/Timer.js
+++ poker/client_src/src/components/Timer.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 
 class Timer extends Component{
-	
+
 	render(){
 
 		return(
 			<div>
-				<h1>Timer Placeholder</h1>
-			</div>
+        <h1 style={{textAlign:'center'}}>
+          Ma partie de Poker en cours
+        </h1> <br /> <br /> <br />
+        
+        <button> clique ici pour gagner 1000 € </button>
+      </div>
+
+
 		)
 	}
 
