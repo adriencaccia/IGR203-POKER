6830e3f9359b86bb0af91677819341953be662f2
Henri <henrilepage@hotmail.fr>
2018-04-06 22:15:02 +0200

    stuff

--- poker/client_src/src/components/Timer.js
+++ poker/client_src/src/components/Timer.js
@@ -11,6 +11,33 @@ class Timer extends Component{
         </h1> <br /> <br /> <br />
         
         <button> clique ici pour gagner 1000 € </button>
+        <br />
+
+        <table>
+        	<tr>
+  				<td>Bonjour, je suis votre première cellule </td>
+ 				<td>je suis votre deuxième cellule </td>
+				<td>je suis votre troisième cellule </td>
+				<td>je suis votre quatrième cellule </td>
+			</tr>
+			<tr>
+  				<td>Bonjour, je suis votre première cellule </td>
+ 				<td>je suis votre deuxième cellule </td>
+				<td>je suis votre troisième cellule </td>
+				<td>je suis votre quatrième cellule </td>
+			</tr>
+        </table>
+		<br />
+		<br /> <span> Blindes </span> <br />
+		"valeur blindes"  +  Timer <br />
+		<br /><span> Joueurs restants </span> <br />
+		liste joueurs <br />
+		<br /><span> classement </span> <br />
+		<br />
+		<br />
+		COUCOU TEY :3
+
+
       </div>
 
 
