9a53a4b503a194dc59a9afa52624e097cb8b9a86
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2018-04-09 01:29:55 +0200

    black chip, new map style

--- poker/client_src/src/components/LeafMap.js
+++ poker/client_src/src/components/LeafMap.js
@@ -6,7 +6,7 @@ import TourneyConfirm from './TourneyConfirm';
 import APIManager from './APIManager';
 
 // const TILES_URL = "http://localhost:8080/styles/poker-style/{z}/{x}/{y}.png";
-const TILES_URL = "http://"+APIManager.getUrl()+":8080/styles/poker-style/{z}/{x}/{y}.png";
+const TILES_URL = "http://"+APIManager.getUrl()+":8080/styles/poker-style2/{z}/{x}/{y}.png";
 // const TILES_URL = "http://137.194.8.91:8080/styles/poker-style/{z}/{x}/{y}.png";
 // const TILES_URL = "http://192.168.137.99:8080/styles/osm-bright/{z}/{x}/{y}.png";
 // const TILES_URL = "https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png";
@@ -21,14 +21,14 @@ const availableIcon = L.icon({
 });
 
 const registeredIcon = L.icon({
-  iconUrl: require('../icons/EXPORTS/SVG/poker_BLEU-01.svg'),
+  iconUrl: require('../icons/EXPORTS/SVG/poker_VERT-01.svg'),
   iconSize: [iconSize, iconSize],
   iconAnchor: [iconSize / 2, iconSize / 2],
   popupAnchor: [0, -iconSize / 2 + iconSize / 8]
 });
 
 const fullIcon = L.icon({
-  iconUrl: require('../icons/EXPORTS/SVG/poker_JAUNE-01.svg'),
+  iconUrl: require('../icons/EXPORTS/SVG/poker_NOIR-01.svg'),
   iconSize: [iconSize, iconSize],
   iconAnchor: [iconSize / 2, iconSize / 2],
   popupAnchor: [0, -iconSize / 2 + iconSize / 8]
@@ -39,7 +39,7 @@ const createClusterCustomIcon = function (cluster) {
     marker.options.icon == registeredIcon);
   if (childRegistered.includes(true)){
     return L.divIcon({
-      html: `<img src=${require('../icons/EXPORTS/SVG/poker_BLEU-02.svg')}
+      html: `<img src=${require('../icons/EXPORTS/SVG/poker_VERT-02.svg')}
             width=${iconSize * 1.1}/>`,
       className: 'marker-cluster-custom',
       iconAnchor: [iconSize / 2, iconSize / 2],
