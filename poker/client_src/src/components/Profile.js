b3256d5f3c6fe28e72a93e61b09f0168482d0f40
Philippe Edwards <philippe.edwards@telecom-paristech.fr>
2018-04-09 18:51:27 +0200

    added profile tabs

--- poker/client_src/src/components/Profile.js
+++ poker/client_src/src/components/Profile.js
@@ -1,33 +1,71 @@
 import React, {Component} from 'react';
-import Token from './Token';
 import APIManager from './APIManager';
+import {Menu} from 'semantic-ui-react';
 
 class Profile extends Component{	
 	constructor(props){
 		super(props);
 		this.state={
-			user:{}
+      user:{},
+      activeItem:"Elo"
 		};
-		this.updateProfile();
 	}
 
+  handleItemClick = (e,{name}) => {
+    this.setState({
+      activeItem:name
+    });
+  }
+
 	updateProfile(){
-		var prom = APIManager.getUserData();
-		console.log(prom);
-		if(prom.then !== undefined){
-			prom.then(response => {
-				this.setState({
-					user:response.data
-				});
-			}).catch(err => {console.log(err)});
-		}
+		APIManager.getUserData().then(response => {
+      if(this.state.user.id!=response.data.id){
+        this.setState({
+          user:response.data
+        });
+      }      
+    }).catch(err => {console.log(err)});
 	}
 
 	render(){
+    //this.updateProfile();
+    const activeItem = this.state.activeItem;
+    var LineChart = require("react-chartjs").Line;
+    var pane;
+    switch(this.state.activeItem){
+      case "Elo":
+        pane=<div>this is tab1</div>;
+        break;
+      case "Parties Récentes":
+        pane=<div>this is tab2</div>;
+        break;
+      case "Classement":
+        pane=<div>this is tab3</div>;
+        break;
+
+    }
+
 		return(
 			<div>
-				<h1 className="app-header">Profile</h1>
-				<p>{Token.get()}</p>
+				<h1 className="app-header">Profile - {APIManager.getUsername()}</h1>
+        <Menu widths={3}>
+          <Menu.Item
+            name='Elo'
+            active={activeItem === 'Elo'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Parties Récentes'
+            active={activeItem === 'Parties Récentes'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Classement'
+            active={activeItem === 'Classement'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+        {pane}
 			</div>
 
 		)
